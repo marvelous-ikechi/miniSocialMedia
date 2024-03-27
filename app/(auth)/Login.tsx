96a826319872a2587438322101187464b0cb7ade
marvelous-ikechi <marvelike7@gmail.com>
2024-03-27 14:18:39 -0700

    navigate to signup; removed unused import

--- app/(auth)/Login.tsx
+++ app/(auth)/Login.tsx
@@ -3,12 +3,11 @@ import ScreenContainer from "@/components/ScreenContainer";
 import tw from "@/lib/tailwind";
 import React, { FunctionComponent } from "react";
 import { Pressable, View } from "react-native";
-import useColors from "@/hooks/useColors";
 import CustomBtn from "@/components/CustomBtn";
 import CustomText from "@/components/CustomText";
+import { router } from "expo-router";
 
 const Login: FunctionComponent = () => {
-  const { textColor } = useColors();
   return (
     <ScreenContainer>
       <View style={tw`flex-1 px-3 `}>
@@ -20,7 +19,10 @@ const Login: FunctionComponent = () => {
             secureTextEntry
           />
           <CustomBtn title="Submit" style={tw`mt-5`} titleColor={"white"} />
-          <Pressable style={tw`w-full mt-3 items-end justify-end`}>
+          <Pressable
+            onPress={() => router.navigate("/(auth)/Signup")}
+            style={tw`w-full mt-3 items-end justify-end`}
+          >
             <CustomText>Signup instead</CustomText>
           </Pressable>
         </View>
