ea72174117e7246cb277751257284506adf88b57
marvelous-ikechi <marvelike7@gmail.com>
2024-03-27 14:22:11 -0700

    removed redundant props

--- app/(auth)/Login.tsx
+++ app/(auth)/Login.tsx
@@ -12,12 +12,8 @@ const Login: FunctionComponent = () => {
     <ScreenContainer>
       <View style={tw`flex-1 px-3 `}>
         <View style={tw`flex-1 items-center  justify-center w-full`}>
-          <CustomTextInput placeholder="Email" placeholderTextColor={"black"} />
-          <CustomTextInput
-            placeholder="Password"
-            placeholderTextColor={"black"}
-            secureTextEntry
-          />
+          <CustomTextInput placeholder="Email" />
+          <CustomTextInput placeholder="Password" secureTextEntry />
           <CustomBtn title="Submit" style={tw`mt-5`} titleColor={"white"} />
           <Pressable
             onPress={() => router.navigate("/(auth)/Signup")}
