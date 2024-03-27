7281e3b6106c3c73fcb810e4b3a32a4d06b62e55
marvelous-ikechi <marvelike7@gmail.com>
2024-03-27 11:56:46 -0700

    used useColorSchemeHook for colors

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,11 +1,14 @@
-import { StyleSheet } from "react-native";
+import { useColorScheme } from "react-native";
 import tw from "@/lib/tailwind";
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
+import Colors from "@/constants/Colors";
 
 export default function TabOneScreen() {
+  const colorScheme = useColorScheme();
+  const backgroundColor = Colors[colorScheme ?? "light"].background.toString();
   return (
-    <View style={tw`bg-black flex-1`}>
+    <View style={tw`bg-${backgroundColor} flex-1`}>
       {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
     </View>
   );
