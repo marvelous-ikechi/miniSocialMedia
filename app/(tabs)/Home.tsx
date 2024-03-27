d0b7ca7a6aadbdd0f7ae2d7d0eb8dbc251b4faa3
marvelous-ikechi <marvelike7@gmail.com>
2024-03-27 11:44:18 -0700

    setup tailwind for styling

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,35 +1,12 @@
 import { StyleSheet } from "react-native";
-
+import tw from "@/lib/tailwind";
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab here</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View style={tw`bg-black flex-1`}>
+      {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
