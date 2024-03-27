9a7c4015d20bc2d49c02317dcd0f62ea11807f1c
marvelous-ikechi <marvelike7@gmail.com>
2024-03-27 14:22:48 -0700

    added Fields to signup screen

--- app/(auth)/Signup.tsx
+++ app/(auth)/Signup.tsx
@@ -1,8 +1,32 @@
+import CustomTextInput from "@/components/CustomTextInput";
+import ScreenContainer from "@/components/ScreenContainer";
+import tw from "@/lib/tailwind";
 import React, { FunctionComponent } from "react";
-import { View } from "react-native";
+import { Pressable, View } from "react-native";
+import CustomBtn from "@/components/CustomBtn";
+import CustomText from "@/components/CustomText";
+import { router } from "expo-router";
 
 const Signup: FunctionComponent = () => {
-  return <View></View>;
+  return (
+    <ScreenContainer>
+      <View style={tw`flex-1 px-3 `}>
+        <View style={tw`flex-1 items-center  justify-center w-full`}>
+          <CustomTextInput placeholder="First Name" />
+          <CustomTextInput placeholder="Last Name" />
+          <CustomTextInput placeholder="Password" secureTextEntry />
+          <CustomTextInput placeholder="Email" />
+          <CustomBtn title="Submit" style={tw`mt-5`} titleColor={"white"} />
+          <Pressable
+            onPress={() => router.navigate("/(auth)/Login")}
+            style={tw`w-full mt-3 items-end justify-end`}
+          >
+            <CustomText>Login instead</CustomText>
+          </Pressable>
+        </View>
+      </View>
+    </ScreenContainer>
+  );
 };
 
 export default Signup;
