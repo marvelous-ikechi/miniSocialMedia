e2baade436a2698e91f475afab391428d8ea8e3d
marvelous-ikechi <marvelike7@gmail.com>
2024-03-27 14:23:22 -0700

    created custom text component

---
+++ components/CustomText.tsx
@@ -0,0 +1,19 @@
+import useColors from "@/hooks/useColors";
+import tw from "@/lib/tailwind";
+import React, { FunctionComponent } from "react";
+import { Text, TextProps } from "react-native";
+
+interface Props extends TextProps {
+  children: React.ReactNode;
+}
+
+const CustomText: FunctionComponent<Props> = ({ children, ...props }) => {
+  const { textColor } = useColors();
+  return (
+    <Text {...props} style={tw`text-${textColor} text-sm`}>
+      {children}
+    </Text>
+  );
+};
+
+export default CustomText;
