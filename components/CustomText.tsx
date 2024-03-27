94b6a0afcb3c7e8a0031017a477899a5538cff33
marvelous-ikechi <marvelike7@gmail.com>
2024-03-27 14:41:43 -0700

    customText props updted

--- components/CustomText.tsx
+++ components/CustomText.tsx
@@ -7,10 +7,14 @@ interface Props extends TextProps {
   children: React.ReactNode;
 }
 
-const CustomText: FunctionComponent<Props> = ({ children, ...props }) => {
+const CustomText: FunctionComponent<Props> = ({
+  children,
+  style,
+  ...props
+}) => {
   const { textColor } = useColors();
   return (
-    <Text {...props} style={tw`text-${textColor} text-sm`}>
+    <Text {...props} style={[tw`text-${textColor} text-sm`, style]}>
       {children}
     </Text>
   );
