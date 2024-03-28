135d8c406cd3fe5ed4c783c4109822c5b100bb4f
marvelous-ikechi <marvelike7@gmail.com>
2024-03-28 11:04:19 -0700

    add firstname and lastname to use as display name during signup

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -3,11 +3,15 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  updateProfile,
 } from "firebase/auth";
 import { useRouter } from "expo-router";
+import { appAuth } from "@/firebaseConfig";
 interface SignupParams {
   email: string;
   password: string;
+  firstName: string;
+  lastName: string;
 }
 
 const useAuth = () => {
@@ -16,26 +20,42 @@ const useAuth = () => {
   const auth = getAuth();
   const { navigate } = useRouter();
 
-  const signup = (params: SignupParams) => {
+  const signup = async ({
+    email,
+    password,
+    firstName,
+    lastName,
+  }: SignupParams) => {
     setLoading(true);
-    createUserWithEmailAndPassword(auth, params.email, params.password)
-      .then(() => {
-        console.log("User account created & signed in!");
-        navigate("/(tabs)/Home");
-      })
-      .catch((error) => {
-        if (error.code === "auth/email-already-in-use") {
-          console.log("That email address is already in use!");
-          setError("That email address is already in use!");
-        } else if (error.code === "auth/invalid-email") {
-          console.log("That email address is invalid!");
-          setError("That email address is invalid!");
-        } else {
-          console.error(error);
-          setError("An unexpected error occurred.");
-        }
-      })
-      .finally(() => setLoading(false));
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      await updateProfile(user, {
+        displayName: `${firstName} ${lastName}`,
+      });
+      console.log(
+        "User account created & signed in with display name:",
+        user.displayName
+      );
+      navigate("/(tabs)/Home");
+    } catch (error: any) {
+      if (error.code === "auth/email-already-in-use") {
+        console.log("That email address is already in use!");
+        setError("That email address is already in use!");
+      } else if (error.code === "auth/invalid-email") {
+        console.log("That email address is invalid!");
+        setError("That email address is invalid!");
+      } else {
+        console.error(error);
+        setError("An unexpected error occurred.");
+      }
+    } finally {
+      setLoading(false);
+    }
   };
 
   const login = (params: SignupParams) => {
@@ -47,6 +67,7 @@ const useAuth = () => {
       })
       .catch((error) => {
         setError("Login error experienced");
+        console.log(error);
       })
       .finally(() => setLoading(false));
   };
