c66fe8738b9562109a7a32987a666c500799c82d
marvelous-ikechi <marvelike7@gmail.com>
2024-03-28 11:15:53 -0700

    updated positions of formfield

--- app/(auth)/Signup.tsx
+++ app/(auth)/Signup.tsx
@@ -63,16 +63,17 @@ const Signup: FunctionComponent = () => {
             value={lastName}
             onChangeText={setLastName}
           />
+
+          <CustomTextInput
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+          />
           <CustomTextInput
             placeholder="Password"
             value={password}
             onChangeText={setPassword}
             secureTextEntry
-          />
-          <CustomTextInput
-            placeholder="Email"
-            value={email}
-            onChangeText={setEmail}
           />
           <CustomBtn
             isLoading={loading}
